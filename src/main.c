e1388a397bf04d7d7e2dfafdaa6d4cdd215cf623
jeankersen <61942315+jeankersen@users.noreply.github.com>
2022-12-16 18:09:33 -0500

    Add files via upload
    get print out

--- src/main.c
+++ src/main.c
@@ -6,29 +6,29 @@ void	*check_death(void  *arg)
 	t_data *data = (t_data *)arg;
 	long nb = data->index;
 	//ft_usleep(1000);
-	printf("ok8\n");
+
 	pthread_mutex_lock(&data->chair[nb].mutex_fork_l);
 	pthread_mutex_lock(&data->mutex_stop);//2-zone proterer de stop
 	if (!is_dead(data, 0) && data->chair[nb].fork < 1) // 3-condition pour la mort et autre-
 	{
-		printf("ok9\n");
+
 		pthread_mutex_unlock(&data->chair[nb].mutex_fork_l);
 		pthread_mutex_unlock(&data->mutex_stop);
 		pthread_mutex_lock(&data->mutex_print);
 		long time = get_time_in_ms();
-		printf("ok10\n");
+
 		if(!is_dead(data, 0) && !data->stop)
 			printf("[%ld] philo %ld               die[%d]\n", time - data->start_time, nb+1, data->chair[nb].fork);
 		pthread_mutex_unlock(&data->mutex_print);
-		printf("ok11\n");
+
 		is_dead(data, 1);
-		printf("ok12\n");
+
 	}
-	printf("ok13\n");
+
 	pthread_mutex_unlock(&data->chair[nb].mutex_fork_l);
-	printf("allo\n");
+
 	pthread_mutex_unlock(&data->mutex_stop);
-	printf("ok14\n");
+
 	return (NULL);
 }
 
@@ -38,29 +38,29 @@ void *ft_philo(void *arg)
 	t_data *data = (t_data *)arg;
 	long nb = data->index;
 	pthread_t	t;
-	printf("ok1\n");
+
 	while (!is_dead(data, 0))
 	{
-		printf("ok2\n");
+
 		pthread_create(&t, NULL, check_death, arg);
 		/*Protected zone by mutex lock*/
-		printf("ok3\n");
+
 		pthread_mutex_lock(&data->chair[nb].mutex_fork_l);
 		long time = get_time_in_ms();
 		pthread_mutex_lock(&data->mutex_print);
 		ft_usleep(10);
 		if(data->chair[nb].fork >= 1 && !is_dead(data, 0))
 		{
-			printf("ok4\n");
+
 			printf("[%ld] philo %ld has taken a fork[%d]\n", time - data->start_time, nb+1, data->chair[nb].fork);
 			data->chair[nb].fork--;
-			printf("ok5\n");
+
 		}
-		printf("ok6\n");
+
 		pthread_mutex_unlock(&data->mutex_print);
 		pthread_mutex_unlock(&data->chair[nb].mutex_fork_l);
 	}
-	printf("ok7\n");
+
 	return NULL;
 }
 
